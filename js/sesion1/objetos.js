eba960b490a39dc3c02876b9a2a870cc00f95620
EsauProgramando <eaveintemillap@alumno.unsm.edu.pe>
2022-04-05 21:19:49 -0500

    Ejercios de js

--- js/sesion1/objetos.js
+++ js/sesion1/objetos.js
@@ -1,4 +1,39 @@
+// const nombre = "José";
+// const apellido = "Velasque";
+// const edad = 23;
+
+// Objeto literal
 const datosPersonales = {
+  nombre: "José",
+  apellido: "Velasque",
+  edad: 23,
+};
+
+// Acceder a un valor
+console.log(datosPersonales.nombre);
+console.log(datosPersonales.apellido);
+console.log(datosPersonales.edad);
+
+console.log(datosPersonales["nombre"]);
+console.log(datosPersonales["apellido"]);
+console.log(datosPersonales["edad"]);
+
+//agregando un valor
+datosPersonales.pais = "Peru";
+console.log(datosPersonales);
+
+//eliminar valor
+delete datosPersonales.pais;
+console.log(datosPersonales);
+
+//destructuring -Accede al valor de segunda forma
+const { nombre, apellido, edad } = datosPersonales;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+const datosPersonales2 = {
   nombre: "jose",
   apellido: "velasque",
   edad: 18,
@@ -8,8 +43,39 @@ const datosPersonales = {
     gol: 2000,
   },
 };
-console.log(datosPersonales);
+console.log(datosPersonales2);
+console.log(datosPersonales2);
+console.log(datosPersonales2.edad);
+console.log(datosPersonales2.estadisticas.asistencia);
 
+const {
+  nombre: name2,
+  apellido: lesName,
+  edad: age,
+  estadisticas: { anotacion, asistencia, gol },
+} = datosPersonales2;
+console.log(`${name2} ${lesName} ${age} ${anotacion}`);
+
+// Congelar un objeto --- no realizar modificaciones
+//Object.freeze(datosPersonales2);
+datosPersonales2.pais = "colombia";
+
+console.log("----------------");
+console.log(datosPersonales2);
+//delete datosPersonales2.estadisticas;
+datosPersonales.nombre = "Juan";
+console.log("----------------");
+
+console.log(Object.isFrozen(datosPersonales2));
+
+// Sellar un objeto ---- no realizar cambios en propipedades pero si en sus valores
+Object.seal(datosPersonales2);
+datosPersonales2.pais = "Perú";
+delete datosPersonales2.estadisticas;
+datosPersonales2.nombre = "Juan";
+console.log("----------------");
+console.log(datosPersonales2);
+console.log("----------------");
 //combinar objetos
 const deportes1 = {
   fulbol: "Fulbol",
